c46f732223fb543069b1e27bb44d19d8f9bb605b
Amaan Sodagar <sodagaramaan786@gmail.com>
2026-02-04 10:46:08 +0530

    commit

--- src/Pages/Employee/Testing/CustomDesign.jsx
+++ src/Pages/Employee/Testing/CustomDesign.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./CustomDesign.scss";
 import ringSketch from "../../../assets/Images/home/custom1.png";
-import mensJewelry from "../../../assets/Images/Home/custom2.png";
+import mensJewelry from "../../../assets/Images/home/custom2.png";
 
 const CustomDesign = () => {
     return (
