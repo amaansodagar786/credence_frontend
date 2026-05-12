d9e8676dbfdebc564f7cbcf62586d1de4620a731
Amaan Sodagar <sodagaramaan786@gmail.com>
2026-05-12 19:10:53 +0530

    commit

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            Sign in
+            Signin
           </motion.span>
         </div>
       </div>
